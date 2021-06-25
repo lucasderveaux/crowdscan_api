699cf937c0da4feee00a489a1c0ade63e2cba219
ljdrveau <ljdrveau.Derveaux@UGent.be>
2021-06-25 13:54:00 +0200

    aanpassingen feedback Brecht van de Vyvere

---
+++ -- SQLite.sql
@@ -0,0 +1,8 @@
+-- SQLite
+CREATE TABLE Gent_langemunt(
+  naam varchar(255),
+  resultTime DateTime,
+  hasSimpleResult double,
+  timeDelta int,
+  madeBySensor varchar(255)
+);
--- api/models/eventStream.js
+++ api/models/eventStream.js
@@ -1,17 +1,20 @@
 const sqlite3 = require('sqlite3').verbose();
 const { OPEN_CREATE,OPEN_READWRITE } = require('sqlite3');
 
-
-let db = new sqlite3.Database('../databank/test2.db',sqlite3.OPEN_READWRITE|sqlite3.OPEN_CREATE,(err)=>{
-  console.log(err.message);
-});
-
-db.close((err)=>{
-  console.log(err.message);
-});
-
 module.exports = function loop(){
+  let db = new sqlite3.Database('./api/databank/observations.db',sqlite3.OPEN_READWRITE|sqlite3.OPEN_CREATE,(err)=>{
+    if(err){
+      console.log(err.message);
+    }else{
+      console.log("db is open");
+    }
+  });
 
+  db.close((err)=>{
+    if(err){
+      console.log(err.message);
+    }
+  });
 
   setInterval((req,res)=>{
     console.log("dag lucas");
@@ -21,6 +24,6 @@ module.exports = function loop(){
 
 
 //stap 1: sqllite databank
-//stap 2: observaties toevogen
+//stap 2: observaties toevoegen
 //stap 3: basisbestand aanmaken
-//stap 4: indien die aan gevraagd wordt de LDE teruggeven met ede juiste gegevens
+//stap 4: indien die aan gevraagd wordt de LDE teruggegeven met de juiste gegevens
